433e97d3aa94b926396102fe24a2190f54555c04
Alen Kopić <a.kopic@naga.com>
2021-08-30 08:41:57 +0200

    added pdf picker

--- tedbottompicker/src/main/java/gun0912/tedbottompicker/TedBottomPicker.java
+++ tedbottompicker/src/main/java/gun0912/tedbottompicker/TedBottomPicker.java
@@ -270,6 +270,9 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
           case GalleryAdapter.PickerTile.GALLERY:
             startGalleryIntent();
             break;
+          case GalleryAdapter.PickerTile.PDF:
+            startPDFIntent();
+            break;
           case GalleryAdapter.PickerTile.IMAGE:
             complete(new MediaPickerEntity(pickerTile.getImageUri()).setType(
                 MediaPickerEntity.MEDIA_TYPE.PICKER));
@@ -322,8 +325,9 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
 
     int px = (int) getResources().getDimension(R.dimen.tedbottompicker_selected_image_height);
     thumbnail.setLayoutParams(new FrameLayout.LayoutParams(px, px));
-
-    if (builder.imageProvider == null) {
+    if (mediaPickerEntity.getType() == MediaPickerEntity.MEDIA_TYPE.PDF) {
+      thumbnail.setBackgroundResource(R.drawable.ic_pdf);
+    } else if (builder.imageProvider == null) {
       Glide.with(getActivity())
           .load(mediaPickerEntity.getUri())
           .thumbnail(0.1f)
@@ -394,7 +398,7 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
     }
 
     if (cameraInent.resolveActivity(getActivity().getPackageManager()) == null) {
-      errorMessage("This Application do not have Camera Application");
+      errorMessage(getString(R.string.no_camera_permission));
       return;
     }
 
@@ -511,7 +515,7 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
     galleryIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
     if (galleryIntent.resolveActivity(getActivity().getPackageManager()) == null) {
-      errorMessage("This Application do not have Gallery Application");
+      errorMessage(getString(R.string.no_gallery_permission));
       return;
     }
 
@@ -522,6 +526,29 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
           public void onActivityResult(int resultCode, Intent data) {
             if (resultCode == Activity.RESULT_OK) {
               onActivityResultGallery(data);
+            }
+          }
+        })
+        .startActivityForResult();
+  }
+
+  private void startPDFIntent() {
+    Intent pdfIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+    pdfIntent.setType("application/pdf");
+    pdfIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+    if (pdfIntent.resolveActivity(getActivity().getPackageManager()) == null) {
+      errorMessage(getString(R.string.no_pdf_permission));
+      return;
+    }
+
+    TedOnActivityResult.with(getActivity())
+        .setIntent(pdfIntent)
+        .setListener(new OnActivityResultListener() {
+          @Override
+          public void onActivityResult(int resultCode, Intent data) {
+            if (resultCode == Activity.RESULT_OK) {
+              onActivityResultPDF(data);
             }
           }
         })
@@ -594,6 +621,19 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
     complete(new MediaPickerEntity(uri).setType(MediaPickerEntity.MEDIA_TYPE.GALLERY));
   }
 
+  private void onActivityResultPDF(Intent data) {
+    Uri uri = data.getData();
+
+    if (uri == null) {
+      errorMessage();
+    }
+
+    final ContentResolver resolver = Objects.requireNonNull(getContext()).getContentResolver();
+    resolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+    complete(new MediaPickerEntity(uri).setType(MediaPickerEntity.MEDIA_TYPE.PDF));
+  }
+
   public interface OnMultiImageSelectedListener {
     void onImagesSelected(ArrayList<MediaPickerEntity> selectedUris);
   }
@@ -618,6 +658,7 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
     public Drawable cameraTileDrawable;
     public Drawable captureVideoTileDrawable;
     public Drawable galleryTileDrawable;
+    public Drawable pdfTileDrawable;
 
     public Drawable deSelectIconDrawable;
     public Drawable selectedForegroundDrawable;
@@ -632,10 +673,12 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
     public boolean showGallery = true;
     public boolean showVideoCapture = true;
     public boolean showVideoMedia = true;
+    public boolean showPDFPicker = false;
     public int peekHeight = -1;
     public int cameraTileBackgroundResId = R.color.tedbottompicker_camera;
     public int captureVideoTileBackgroundResId = R.color.tedbottompicker_camera;
     public int galleryTileBackgroundResId = R.color.tedbottompicker_gallery;
+    public int pdfTileBackgroundResId = R.color.tedbottompicker_gallery;
 
     public String title;
     public boolean showTitle = true;
@@ -665,6 +708,7 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
       setCameraTile(R.drawable.baseline_photo_camera_white_36);
       setGalleryTile(R.drawable.baseline_collections_white_36);
       setCaptureVideoTile(R.drawable.baseline_videocam_white_48);
+      setPDFTile(R.drawable.ic_pdf);
       setSpacingResId(R.dimen.tedbottompicker_grid_layout_margin);
     }
 
@@ -675,6 +719,11 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
 
     public Builder setGalleryTile(@DrawableRes int galleryTileResId) {
       setGalleryTile(ContextCompat.getDrawable(context, galleryTileResId));
+      return this;
+    }
+
+    public Builder setPDFTile(@DrawableRes int pdfTileResId) {
+      setPDFTile(ContextCompat.getDrawable(context, pdfTileResId));
       return this;
     }
 
@@ -711,8 +760,18 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
       return this;
     }
 
+    public Builder setShowPDFPicker(boolean showPDFPicker) {
+      this.showPDFPicker = showPDFPicker;
+      return this;
+    }
+
     public Builder setGalleryTile(Drawable galleryTileDrawable) {
       this.galleryTileDrawable = galleryTileDrawable;
+      return this;
+    }
+
+    public Builder setPDFTile(Drawable pdfTileDrawable) {
+      this.pdfTileDrawable = pdfTileDrawable;
       return this;
     }
 
@@ -942,10 +1001,11 @@ public class TedBottomPicker extends BottomSheetDialogFragment {
     }
 
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({MediaType.IMAGE, MediaType.VIDEO})
+    @IntDef({MediaType.IMAGE, MediaType.VIDEO, MediaType.PDF})
     public @interface MediaType {
       int IMAGE = 1;
       int VIDEO = 2;
+      int PDF = 3;
     }
   }
 }
